0f1ce7a21db0ba19a46c08da7a3ede34ad03fc40
Franco Ortega <francopdx@gmail.com>
2022-07-09 23:18:19 -0700

    add header to Layout

--- pages/index.js
+++ pages/index.js
@@ -1,16 +1,5 @@
-
 import Home from '../components/home/Home';
-import styles from '../styles/Home.module.css';
 
 export default function HomePage() {
-  return (
-    <div className={styles.container}>
-      
-
-      <main>
-        Home Page
-        <Home />
-      </main>
-    </div>
-  )
+  return <Home />;
 }
