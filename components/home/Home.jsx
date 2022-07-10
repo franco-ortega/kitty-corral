2764f504f35fdfc198afbb1de74955d72ab92b66
Franco Ortega <francopdx@gmail.com>
2022-07-10 00:04:55 -0700

    move to Corral on submit

--- components/home/Home.jsx
+++ components/home/Home.jsx
@@ -1,12 +1,18 @@
+import Router from 'next/router';
 import styles from './Home.module.css';
 
 const Home = ({ setCorralSize }) => {
+  const onCorralSizeSubmit = (e) => {
+    e.preventDefault();
+    Router.push('/corral');
+  };
+
   return (
     <div className={styles.Home}>
       <h2>Landing Page</h2>
       <form action="">
         <input type="number" onChange={(e) => setCorralSize(e.target.value)} />
-        <button>Submit</button>
+        <button onClick={onCorralSizeSubmit}>Submit</button>
       </form>
     </div>
   );
