6faaaf02d3abf3354ccf1e6fb294d611dc744d0d
Franco Ortega <francopdx@gmail.com>
2022-07-10 00:50:07 -0700

    add Slot component

--- components/corral/Corral.jsx
+++ components/corral/Corral.jsx
@@ -1,19 +1,28 @@
 import { useEffect, useState } from 'react';
 import Kitty from '../kitty/Kitty';
+import Slot from '../slot/Slot';
 import styles from './Corral.module.css';
 
 const Corral = ({ corralSize }) => {
   const [kittyAmount, setKittyAmount] = useState(corralSize);
-  const [kittyList, setKittyList] = useState([]);
+  // const [kittyList, setKittyList] = useState([]);
+  const [slots, setslots] = useState([]);
 
-  if(!kittyList.length) {
+  // if(!kittyList.length) {
+  //   for (let i = 0; i < kittyAmount; i++) {
+  //     kittyList.push(<Kitty key={i} />);
+  //     console.log('LOOP: ', i);
+  //   }
+  // }
+
+  if(!slots.length) {
     for (let i = 0; i < kittyAmount; i++) {
-      kittyList.push(<Kitty key={i} />);
+      slots.push(<Slot key={i} />);
       console.log('LOOP: ', i);
     }
   }
 
-  return <div className={styles.Corral}>{kittyList}</div>;
+  return <div className={styles.Corral}>{slots}</div>;
 };
 
 export default Corral;
