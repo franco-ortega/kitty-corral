27c30ec513b5458123a60082db5a934f4254a62b
Franco Ortega <francopdx@gmail.com>
2022-07-15 18:03:38 -0700

    add About component

---
+++ components/corral/about/About.jsx
@@ -0,0 +1,11 @@
+import styles from './About.module.css';
+
+const About = () => {
+  return (
+    <div className={styles.About}>
+      <h2>Coming Soon</h2>
+    </div>
+  );
+};
+
+export default About;
